2d82426e47f6d5f8dbcc76fb6c6c67f94e25e397
Atuxe <atuxe@atuxe.com>
2015-12-29 17:44:53 +0800

    add main.js add index.html

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+let mainWindow;
+
+app.on('window-all-closed', function () {
+  if (process.platform != 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('ready', function () {
+  mainWindow = new BrowserWindow({width:800, height:600});
+  var url = 'file://' + __dirname + '/index.html';
+  mainWindow.loadUrl(url);
+  mainWindow.webContents.openDevTools();
+  mainWindow.on('closed', function() {
+    mainWindow = null;
+  })
+});
